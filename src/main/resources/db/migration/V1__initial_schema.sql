a928e10d56fb7e10f4c887ae015ef44d99ef2cc6
Prakash <kdreamz20@gmail.com>
2026-08-10 17:39:10 +0800

    Add Flyway initial schema

---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE roles (
+    id UUID PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE  user_roles(
+    user_id UUID NOT NULL,
+    role_id UUID NOT NULL,
+
+    PRIMARY KEY(user_id, role_id),
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id),
+
+    CONSTRAINT fk_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+);
