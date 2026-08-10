48f662f7c894b56568352cdb74b7afb99bf7bd4f
Prakash <kdreamz20@gmail.com>
2026-08-10 19:00:38 +0800

    Seed system roles

---
+++ src/main/java/com/example/capitalmarkets/tradesettlement/user/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.capitalmarkets.tradesettlement.user;
+
+public class UserServiceImpl {
+}
---
+++ src/main/resources/db/migration/V2__seed_roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles(id, name)
+VALUES
+(gen_random_uuid(), 'ROLE_ADMIN'),
+(gen_random_uuid(), 'ROLE_TRADER'),
+(gen_random_uuid(), 'ROLE_OPERATIONS')
