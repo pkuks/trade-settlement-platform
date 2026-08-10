3b912728a6ec512bb6f8be414a68dfec428772b0
Prakash <kdreamz20@gmail.com>
2026-08-10 16:00:44 +0800

    Initialize Spring Boot project and Added PostgreSQL docker configuration

---
+++ src/main/java/com/example/capitalmarkets/tradesettlement/TradeSettlementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.capitalmarkets.tradesettlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSettlementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSettlementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/capitalmarkets/tradesettlement/TradeSettlementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capitalmarkets.tradesettlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSettlementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
