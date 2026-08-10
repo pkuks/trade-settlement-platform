9c1dff4d38b0bfc71afb20b74637dcd1cd243eeb
Prakash <kdreamz20@gmail.com>
2026-08-10 19:01:47 +0800

    Add user creation DTOs

--- src/main/java/com/example/capitalmarkets/tradesettlement/user/CreateUserRequest.java
+++ src/main/java/com/example/capitalmarkets/tradesettlement/user/CreateUserRequest.java
@@ -1,4 +1,19 @@
 package com.example.capitalmarkets.tradesettlement.user;
 
-public record CreateUserRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+public record CreateUserRequest(
+        @NotBlank
+        String username,
+
+        @NotBlank
+        @Size(min=8)
+        String password,
+
+        @NotEmpty
+        Set<String> roles) {
 }
--- src/main/java/com/example/capitalmarkets/tradesettlement/user/UserResponse.java
+++ src/main/java/com/example/capitalmarkets/tradesettlement/user/UserResponse.java
@@ -1,4 +1,12 @@
 package com.example.capitalmarkets.tradesettlement.user;
 
-public record UserResponse() {
+import java.util.Set;
+import java.util.UUID;
+
+public record UserResponse(
+        UUID id,
+        String username,
+        boolean enabled,
+        Set<String> roles
+) {
 }
